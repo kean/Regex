dc2e8493cc23e4e7fb584b1bd61976a6fbe0043f
kean <grebenyuk.alexander@gmail.com>
2019-09-14 12:08:26 +0200

    Cleanup

--- Source/Parser.swift
+++ Source/Parser.swift
@@ -101,34 +101,16 @@ func zip<A, B>(_ a: Parser<A>, _ b: Parser<B>) -> Parser<(A, B)> {
     }
 }
 
-func zip<A, B, C>(
-    _ a: Parser<A>,
-    _ b: Parser<B>,
-    _ c: Parser<C>
-) -> Parser<(A, B, C)> {
-    zip(a, zip(b, c))
-        .map { a, bc in (a, bc.0, bc.1) }
+func zip<A, B, C>(_ a: Parser<A>, _ b: Parser<B>, _ c: Parser<C>) -> Parser<(A, B, C)> {
+    zip(a, zip(b, c)).map { a, bc in (a, bc.0, bc.1) }
 }
 
-func zip<A, B, C, D>(
-    _ a: Parser<A>,
-    _ b: Parser<B>,
-    _ c: Parser<C>,
-    _ d: Parser<D>
-) -> Parser<(A, B, C, D)> {
-    zip(a, zip(b, c, d))
-        .map { a, bcd in (a, bcd.0, bcd.1, bcd.2) }
+func zip<A, B, C, D>(_ a: Parser<A>, _ b: Parser<B>, _ c: Parser<C>, _ d: Parser<D>) -> Parser<(A, B, C, D)> {
+    zip(a, zip(b, c, d)).map { a, bcd in (a, bcd.0, bcd.1, bcd.2) }
 }
 
-func zip<A, B, C, D, E>(
-    _ a: Parser<A>,
-    _ b: Parser<B>,
-    _ c: Parser<C>,
-    _ d: Parser<D>,
-    _ e: Parser<E>
-) -> Parser<(A, B, C, D, E)> {
-    zip(a, zip(b, c, d, e))
-        .map { a, bcde in (a, bcde.0, bcde.1, bcde.2, bcde.3) }
+func zip<A, B, C, D, E>(_ a: Parser<A>, _ b: Parser<B>, _ c: Parser<C>, _ d: Parser<D>, _ e: Parser<E>) -> Parser<(A, B, C, D, E)> {
+    zip(a, zip(b, c, d, e)).map { a, bcde in (a, bcde.0, bcde.1, bcde.2, bcde.3) }
 }
 
 /// Returns the first match or `nil` if no matches are found.
